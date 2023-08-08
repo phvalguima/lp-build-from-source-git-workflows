98ba06507885ff20bf7c97f31e042d17d6f173c0
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2023-08-08 18:44:51 +0200

    Fixed remote

--- opensearch/lp_change_remotes_to_public_project.sh
+++ opensearch/lp_change_remotes_to_public_project.sh
@@ -6,11 +6,12 @@ find . -maxdepth 1 \( -name "opensearch*" -o -name "python-*" \) -type d | awk '
     pushd "${project}" || exit 1
 
     if [[ "${project}" == *opensearch-build ]]; then
+        popd || exit 1
         continue
     fi
 
     git remote remove launchpad
-    git remote add launchpad "https://git.launchpad.net/~medib/opensearch-project-components/+git/${project#./}"
+    git remote add launchpad "git+ssh://medib@git.launchpad.net/~medib/opensearch-project-components/+git/${project#./}"
 
     popd || exit 1
 
