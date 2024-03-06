b1f057615e38addc1b1f5aacaec4410351b84e1d
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2024-03-06 16:21:46 +0100

    Add lp scripts

---
+++ opensearch/03_lp_change_gradle_distro.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# declare -a VERSIONS=("2.9.0" "2.8.0")
+declare -a VERSIONS=("2.11.1")
+
+USERNAME=pguimaraes
+
+
+pushd ../..
+
+remote_url="distributionUrl=https\\\:\/\/services.gradle.org\/distributions"
+jfrog_url="distributionUrl=https\\\:\/\/canonical.jfrog.io\/artifactory\/dataplatform-generic-stable-local\/gradle"
+
+find . -maxdepth 1 -name "opensearch*" -type d | awk '{print $1}' | while read -r project; do
+    pushd "${project}" || exit 1
+
+    if [[ "${project}" == *python ]]; then
+        continue
+    fi
+
+    if [[ "${project}" == *opensearch-performance-analyzer-rca ]]; then
+        git checkout main
+        sed -i -e "s/^${remote_url}\+/${jfrog_url}/g" gradle/wrapper/gradle-wrapper.properties
+        git add .
+        git commit -m "changed gradle distro url"
+        git push launchpad
+
+        popd || exit 1
+        continue
+    fi
+
+    for version in "${VERSIONS[@]}"; do
+        git checkout "lp-${version}"
+
+        gradle_wrapper="gradle/wrapper/gradle-wrapper.properties"
+        if [[ "${project}" == *notifications* ]]; then
+            gradle_wrapper="notifications/${gradle_wrapper}"
+        fi
+
+        sed -i -e "s/^${remote_url}\+/${jfrog_url}/g" "${gradle_wrapper}"
+        sed -i -e "s/^# distributionSha256Sum\+/distributionSha256Sum/g" "${gradle_wrapper}"
+
+        git add .
+        git commit -m "changed gradle distro url"
+        git push launchpad
+
+        version_tag="$(git tag -l --sort=version:refname "lp-v${version}.*" | tail -1)"
+        git tag "${version_tag}" --force
+        git push launchpad "${version_tag}" --force
+
+    done
+
+    popd || exit 1
+
+done
